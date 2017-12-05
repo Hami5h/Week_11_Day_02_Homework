c8bd98476a4ba86dc8d0fe6db0b9ee71780ff583
Hamish <hamishfpso@gmail.com>
2017-12-05 17:22:28 +0000

    Increase Dinosaur offspring per year passed

--- dinosaur.js
+++ dinosaur.js
@@ -1,15 +1,20 @@
-const Dinosaur = function (type, offspring) {
+const Dinosaur = function (type, offspring, year) {
   this.type = type;
   this.offspring = offspring;
+  this.year = year;
 }
 
 Dinosaur.prototype.hasType = function() {
   return this.type;
 }
 
-Dinosaur.prototype.increaseOffspring = function(offspring) {
-  this.offspring += offspring;
-  return this.offspring;
+Dinosaur.prototype.increaseOffspring = function(offspring, year) {
+  yearCount = year - this.year;
+  if(yearCount != 0) {
+    this.offspring += (offspring * yearCount);
+    return this.offspring;
+  }
+
 }
 
 module.exports = Dinosaur;
--- specs/dinosaur_specs.js
+++ specs/dinosaur_specs.js
@@ -3,13 +3,18 @@ const assert = require('assert');
 
 describe('Dinosaur', function() {
   it('Type of Dinosaur', function() {
-    const dinosaur = new Dinosaur("Tyrannosaurus");
+    const dinosaur = new Dinosaur("Tyrannosaurus", 2, 1);
     assert.strictEqual(dinosaur.type, "Tyrannosaurus");
   });
 
   it('Produce offspring', function() {
-    const dinosaur = new Dinosaur("Tyrannosaurus", 2);
-    assert.strictEqual(dinosaur.increaseOffspring(2), 4);
+    const dinosaur = new Dinosaur("Tyrannosaurus", 2, 1);
+    assert.strictEqual(dinosaur.increaseOffspring(2, 0), 0);
+  });
+
+  it('Increase offspring every year', function() {
+    const dinosaur = new Dinosaur("Tyrannosaurus", 2, 1);
+    assert.strictEqual(dinosaur.increaseOffspring(2, 4), 8);
   });
 
 });
