ad7b0a467bdd994fec1081a217ea66fc1cd1f7f8
Hamish <hamishfpso@gmail.com>
2017-12-05 18:02:05 +0000

    Remove a Dinosaur from an enclosure

--- park.js
+++ park.js
@@ -6,4 +6,17 @@ Park.prototype.enclosureSize = function() {
   return this.enclosure.length;
 }
 
+Park.prototype.addDinosaur = function(dinosaur) {
+  this.enclosure.push(dinosaur);
+}
+
+Park.prototype.removeDinosaur = function(type) {
+  for (dinosaur of this.enclosure) {
+    if (dinosaur.type === type) {
+      index = this.enclosure.indexOf(dinosaur);
+        this.enclosure.splice(index, 1);
+      }
+    }
+  }
+
 module.exports = Park;
--- specs/park_specs.js
+++ specs/park_specs.js
@@ -9,4 +9,24 @@ describe('Park', function() {
     assert.strictEqual(park.enclosureSize(), 0);
   });
 
+  it("Should be able to add Dinosaur to enclosure", function() {
+    const park = new Park();
+    const dinosaur = new Dinosaur('Velociraptor', 1, 1);
+    park.addDinosaur(dinosaur)
+    assert.strictEqual(park.enclosureSize(), 1);
+  });
+
+  it("Should be able to remove Dinosaur from enclosure", function() {
+    const park = new Park();
+    const dinosaur1 = new Dinosaur('Velociraptor', 1, 1);
+    const dinosaur2 = new Dinosaur('Tyrannosaurus', 2, 1);
+    const dinosaur3 = new Dinosaur('Triceratops', 3, 1);
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    assert.strictEqual(park.enclosureSize(), 3);
+    park.removeDinosaur('Tyrannosaurus');
+    assert.strictEqual(park.enclosureSize(), 2);
+    assert.deepStrictEqual(park.enclosure, [dinosaur1, dinosaur3])
+  });
 });
